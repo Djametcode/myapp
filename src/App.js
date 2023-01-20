46a4f7e5ca8d1fce020ec0411e9a31b72c50a531
Djametcode <kampretsiji1@gmai.com>
2023-01-20 16:02:32 +0700

    cmm

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { useContext } from "react";
 import { createContext } from "react";
 import { useState } from "react";
 import "./App.css"
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
 const itemContext = createContext(null)
 
